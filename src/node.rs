6e5e223b6892afddae535eaddc60cde3176f16f9
Loïc Pinsard <muream@gmail.com>
2019-10-15 20:10:32 +0200

    basic node stuff

--- src/node.rs
+++ src/node.rs
@@ -1,8 +1,39 @@
-use crate::attribute::Attribute;
+use crate::attribute::{Attribute, AttributeType};
 use crate::plug::Plug;
 
-struct Node<'a> {
+struct Node<'a, T>
+where
+    T: AttributeType,
+{
     name: String,
-    plugs: Vec<Plug<'a>>,
-    attributes: Vec<Box<Attribute>>,
+    plugs: Vec<Plug<'a, T>>,       // should be a hash-map with uuids
+    attributes: Vec<Attribute<T>>, // should be a hash-map with uuids
+}
+
+// related functions
+impl<T> Node<'_, T>
+where
+    T: AttributeType,
+{
+    #[allow(dead_code)]
+    pub fn new(name: String) -> Self {
+        Node {
+            name: name,
+            plugs: vec![],
+            attributes: vec![],
+        }
+    }
+}
+
+// methods
+impl<T> Node<'_, T>
+where
+    T: AttributeType,
+{
+    #[allow(dead_code)]
+    pub fn add_attribute(&mut self, name: String, value: T) -> &Attribute<T> {
+        let attribute = Attribute::new(name, value);
+        self.attributes.push(attribute);
+        self.attributes.last().unwrap()
+    }
 }
